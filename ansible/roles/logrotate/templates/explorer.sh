9a6ddb9d36354464cd66faa62f8f17f5525bc30f
Branislav Kojic <branislav.kojic@ethernal.tech>
2024-02-07 10:22:02 +0100

    Added logrotate

---
+++ ansible/roles/logrotate/templates/explorer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo chown root:root /var/log
+
+echo "/var/log/syslog {
+    rotate 3
+	create
+    hourly
+    missingok
+    notifempty
+    delaycompress
+    compress
+    postrotate
+       sudo /usr/lib/rsyslog/rsyslog-rotate
+    endscript
+}" | sudo tee /etc/logrotate.d/custom_logrotate_hourly.conf >/dev/null
+
+(crontab -l 2>/dev/null | grep -Fq "*/20 * * * * logrotate -f /etc/logrotate.d/custom_logrotate_hourly.conf") || (crontab -l 2>/dev/null; echo "*/20 * * * * logrotate -f /etc/logrotate.d/custom_logrotate_hourly.conf") | crontab -
