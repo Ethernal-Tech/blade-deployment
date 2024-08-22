9ad2722eac526dce7f834caaaeeaf9669359cb19
Branislav Kojic <branislav.kojic@ethernal.tech>
2024-08-22 10:13:19 +0200

    Output LB for SC Verifier HTTP API

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "aws_lb_explorer" {
   value = module.elb.aws_lb_explorer_domain
 }
 
+output "aws_lb_smart_contract_veririfer" {
+  value = module.elb.aws_lb_smart_contract_verifier_domain
+}
+
 output "aws_lb_faucet" {
   value = module.elb.aws_lb_faucet_domain
 }
