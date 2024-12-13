d4545b3a4c93f30a3cb4dd34605430c91ecfe661
visnja <milicamladjenovic95@gmail.com>
2024-12-13 11:01:53 +0100

    Add output for script

--- modules/bootstrap/scripts/local.sh
+++ modules/bootstrap/scripts/local.sh
@@ -7,7 +7,7 @@ main() {
     aws s3 cp s3://$DEPLOYMENT_NAME-state-bucket/$DEPLOYMENT_NAME/bootstrap.sh ./bootstrap.sh && \
     aws ssm get-parameter --region $REGION --name /$DEPLOYMENT_NAME/config.json  --query Parameter.Value --output text > ./config.json
     chmod +x ./bootstrap.sh && \
-    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY ./bootstrap.sh 2>&1 bootstrap_output.txt
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY ./bootstrap.sh 2>&1 ./bootstrap_output.txt
     popd
     popd
 }
