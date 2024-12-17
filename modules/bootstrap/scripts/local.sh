0c24da7428f4d41c9f2a570aa3a9936cbf12db5b
visnja <milicamladjenovic95@gmail.com>
2024-12-17 10:51:48 +0100

    Add env vars before s3 bucket

--- modules/bootstrap/scripts/local.sh
+++ modules/bootstrap/scripts/local.sh
@@ -4,8 +4,8 @@ main() {
     pushd /tmp
     mkdir $RANDOM_PET
     pushd $RANDOM_PET
-    aws s3 cp s3://$DEPLOYMENT_NAME-state-bucket/$DEPLOYMENT_NAME/bootstrap.sh ./bootstrap.sh && \
-    aws ssm get-parameter --region $REGION --name /$DEPLOYMENT_NAME/config.json  --query Parameter.Value --output text > ./config.json
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws s3 cp s3://$DEPLOYMENT_NAME-state-bucket/$DEPLOYMENT_NAME/bootstrap.sh ./bootstrap.sh && \
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY aws ssm get-parameter --region $REGION --name /$DEPLOYMENT_NAME/config.json  --query Parameter.Value --output text > ./config.json
     chmod +x ./bootstrap.sh && \
     AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY ./bootstrap.sh > ./bootstrap_output.txt 2>&1
     popd
